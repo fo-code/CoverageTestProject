859ed2c4bf9aca65767c36954c0734c6ba2fe00b
Florian Orendi <orendi@hm.edu>
2021-10-23 12:58:15 +0200

    Test commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,12 @@ public class Main {
 
     public static void main(String... args) {
         System.out.println();
-        System.out.println();
         tmp();
         tmp();
         tmp();
         tmp();
         tmp();
+        tmp2();
     }
 
     public static void tmp() {
@@ -15,6 +15,10 @@ public class Main {
         System.out.println();
         System.out.println();
         System.out.println();
+        System.out.println();
+    }
+
+    public static void tmp2() {
         System.out.println();
     }
 }
