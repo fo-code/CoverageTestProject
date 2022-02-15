248e30d58e08ceb8445cc42f402e2591fab94627
Florian Orendi <orendi@hm.edu>
2022-02-15 18:47:37 +0100

    Coverage Test Commit

--- src/main/java/test/example/duplicates/Test99.java
+++ src/main/java/test/example/duplicates/Test99.java
@@ -5,4 +5,16 @@ public class Test99 {
     public void a() {
         System.out.println("test");
     }
+
+    public void b() {
+        System.out.println("test");
+    }
+
+    public void c() {
+        System.out.println("test");
+    }
+
+    public void d() {
+        System.out.println("test");
+    }
 }
