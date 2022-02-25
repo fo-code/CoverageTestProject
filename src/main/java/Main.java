2158922cd2684dccac7b88a4f1fcc92521cdeb60
Florian Orendi <orendi@hm.edu>
2022-02-25 11:24:58 +0100

    Coverage Test Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,9 +23,13 @@ public class Main {
 
     public static void b() {
         System.out.println();
+        System.out.println();
+        System.out.println();
     }
 
     public static void c() {
+        System.out.println();
+        System.out.println();
         System.out.println();
     }
 }
